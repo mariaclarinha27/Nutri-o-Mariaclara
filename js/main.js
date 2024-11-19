28b5aa98cc36057e749f0f8fa789bd7ba0c800fb
mariaclarinha27 <gokujosiana@gmail.com>
2024-11-19 00:22:36 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var titulo = document.querySelector("h1");
+titulo.textContent = "Maria Clara Nutricionista";
+
+var subtitulo = document.querySelector("h2");
+subtitulo.textContent = "Pacientes Cadastrados";
+
+var paciente = document.querySelector("#primeiro-paciente");
+console.log(paciente);
